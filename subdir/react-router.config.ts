fa9736088f2c673aed5f0907bb021def2afb2a2b
Abdullah Ammar <abdullah.ammar@accelbyte.net>
2025-11-19 13:05:53 +0700

    fix: add basename in react router

--- subdir/react-router.config.ts
+++ subdir/react-router.config.ts
@@ -4,5 +4,6 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  prerender: ['/']
+  prerender: ['/'],
+  basename: '/extend-apps-explorer/'
 } satisfies Config;
